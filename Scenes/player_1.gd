420c24527d65b44ce52173e87e78cd23b25547eb
Aryan Zihan Khan <aryanzihanalreadytaken@gmail.com>
2024-10-16 05:13:18 +0600

    enemyadding

--- Scenes/player_1.gd
+++ Scenes/player_1.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+var attack_ip = false
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -550.0
@@ -8,6 +13,11 @@ const JUMP_VELOCITY = -550.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	if health >= 0:
+		player_alive = false
+		$respawn.start()
+	
+	enemy_attack()
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -39,3 +49,30 @@ func _physics_process(delta: float) -> void:
 
 
 	move_and_slide()
+
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy") :
+		enemy_inattack_range = true
+
+func player():
+	pass
+
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy") :
+		enemy_inattack_range = false
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown:
+		health = health - 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
+
+
+func _on_respawn_timeout() -> void:
+	get_tree().change_scene_to_file('res://Scenes/game.tscn')
