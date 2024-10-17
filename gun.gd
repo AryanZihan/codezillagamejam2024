d09c5995c14cd81a9d2920fe67f1436b2259e8f0
gopibahu45 <tasniar@yahoo.com>
2024-10-17 17:06:09 +0600

    minions assemble

--- gun.gd
+++ gun.gd
@@ -2,18 +2,18 @@ extends Area2D
 
 
 func _physics_process(delta: float) -> void:
-	var player_in_range = get_overlapping_bodies()
-	if player_in_range.size() > 0:
-		var target_player = player_in_range.front()
-		look_at(target_player.global_position)
-		
-func shoot():
-		const BULLET = preload("res://bullet.gd")
-		var new_bullet = BULLET.NOTIFICATION_SCENE_INSTANTIATED
-		new_bullet.global_position = %"shooting point".global_position	
-		new_bullet.global_rotation = %"shooting point".global_rotation
-		%"shooting point".add_child(new_bullet)
+    var enemies_in_range = get_overlapping_bodies()
+    if enemies_in_range.size() > 0:
+        var target_enemy = enemies_in_range[0]
+        look_at(target_enemy.global_position)
 
+
+func shoot():
+    const BUllET = preload("res://bullet.tscn")
+    var new_bullet = BUllET.instantiate()
+    
+    
 
 func _on_timer_timeout() -> void:
-	shoot()
+    shoot()
+     
