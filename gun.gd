27dd607671ffa9e38aa67acece8e64b57e8a86d9
gopibahu45 <tasniar@yahoo.com>
2024-10-17 14:45:48 +0600

    added intro

--- gun.gd
+++ gun.gd
@@ -9,11 +9,11 @@ func _physics_process(delta: float) -> void:
 		
 func shoot():
 		const BULLET = preload("res://bullet.gd")
-		var new_bullet = BULLET.DUPLICATE_USE_INSTANTIATION
+		var new_bullet = BULLET.NOTIFICATION_SCENE_INSTANTIATED
 		new_bullet.global_position = %"shooting point".global_position	
 		new_bullet.global_rotation = %"shooting point".global_rotation
 		%"shooting point".add_child(new_bullet)
 
 
-func _on_timer_timeout() -> void: 
+func _on_timer_timeout() -> void:
 	shoot()
